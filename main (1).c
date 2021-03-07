c489cf39d57f9fa1720f5c1b19511db687676dce
Ilya Nyrkov <71572784+IlyaNyrkov@users.noreply.github.com>
2021-03-07 23:27:29 +0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+
+enum roles {
+    developer = 0,
+    client,
+    product_owner,
+    product_manager
+};
+typedef enum roles roles;
+struct participants {
+    roles role;
+    char name[15];
+    char surname[30];
+    size_t importance; // 1 ... 10
+};
+typedef struct participants participants;
+
+//requests
+//SHOW <role>
+void help() {
+    printf("COMMAND LIST: \n\tHELP - command info, ");
+    printf("\n\tSHOW [-all] [-name <participant_name>] [-surname <participant_name>]");
+    printf("\n\t[-importance < LESS || MORE > <number>] - show info depending on keys");
+    printf("\nOPTIONS:");
+    printf("\n\t-surname, -importance, -name, -role can be used at the same time");
+    printf("\n\t-all\n\t\tshow all project participants");
+    printf("\n\t-surname <participant_name>\n\t\tshow participants with <participant_name>");
+    printf("\n\t-importance <LESS || MORE > <number>\n\t\tshow participants with importance level");
+    printf("\n\t\tMORE or LESS to written <number>, if there is no <MORE or LESS> prints participants");
+    printf("\n\t\twith equal to <number> importance level");
+}
+
+void print_particip_info(const participants* participant) {
+    printf("%s, %s,\nrole: %s,\nimportance level: %d", 
+    participant->name, participant->surname, participant->importance, participant->role);
+}
+
+int main()
+{
+    
+    help();
+
+    return 0;
+}

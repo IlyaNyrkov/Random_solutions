d55f4647eb21dffaf286b7f02b31ecc751058baa
Ilya Nyrkov <71572784+IlyaNyrkov@users.noreply.github.com>
2021-03-12 17:25:28 +0300

    Update main (1).c

--- main (1).c
+++ main (1).c
@@ -1,102 +1,129 @@
+
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-//Создать структуру для хранения информации о заинтересованных сторонах проекта:
-//их роли (заказчик, покупатель, разработчик и пр.), наименовании или имени,
-// степени влияния и т.д. Составить с ее использованием программу 
-//вывода информации обо всех заинтересованных
-
-
-struct participants {
-    char* role; //developer client product_owner product_manager
+#include <string.h>
+#define MEM_ALLOC_ERR -1
+struct Person {
     char* name;
     char* surname;
-    size_t importance; // 1 ... 10
+    char* role;
+    size_t importance;
+    
 };
-typedef struct participants participants;
+typedef struct Person Person;
 
-//requests
-//SHOW <role>
-void help() {
-    printf("COMMAND LIST: \n    HELP - show command info,\n    EXIT - exit from program,");
-    printf("\n    SHOW [-all] [-name <participant_name>] [-surname <participant_name>]");
-    printf("\n    [-importance < LESS || MORE > <number>] - show info depending on keys");
-    printf("\nOPTIONS for SHOW:");
-    printf("\n    -surname, -importance, -name, -role\n        can be used at the same time");
-    printf("\n    -all\n        show all project participants");
-    printf("\n    -surname <participant_name>\n        show participants with <participant_name>");
-    printf("\n    -importance <LESS || MORE > <number>\n        show participants with importance level");
-    printf("\n        MORE or LESS to written <number>, if there is no\n        <MORE or LESS> prints participants");
-    printf("\n        with equal to <number> importance level\n");
+void show_person_info(Person* person) {
+    printf("name: %20s\nsurname: %30s\nrole: %15s\nimportance: %zu", 
+        person->name, person->surname, person->role, person->importance);
+    
 }
 
-int print_particip_info(const participants* participant) {
-    printf("%s %s\nrole: %s\nimportance level: %zu\n",
-        participant->name, participant->surname, participant->role, participant->importance);
-}
+size_t name_size = 20;
+size_t surname_size = 30;
+size_t role_size = 15;
 
-int fill_participant (char* buffer, participants* participant) {
-    char* data =  strsep(&buffer, " ");
-    participant->role = data;
-    data =  strsep(&buffer, " ");
-    participant->name = data;
-    data =  strsep(&buffer, " ");
-    participant->surname = data;
-    data =  strsep(&buffer, " ");
-    participant->importance = strtol(data, NULL, 10);
-    return 0;
-}
-
-int make_participants (int particip_count, participants* particips, char* buffer[]) {
-    particips = (participants*)malloc(sizeof(participants) * particip_count);
-    if (particips == NULL) {
-        printf("Can't allocate memory for participants");
-        return 0;
+int mem_alloc_person(Person* person, size_t name_sz,
+    size_t surname_sz, size_t role_sz) {
+    person->name = malloc(name_sz * sizeof(char));
+    if (person->name == NULL) {
+        return MEM_ALLOC_ERR;
     }
-    for (size_t i = 0; i < particip_count; i++) {
-        getline(&buffer[i], &strlen(buffer[i]), stdin);
-        fill_participant(buffer[i], &particips[i]);
+    person->surname = malloc(name_sz * sizeof(char));
+    if (person->surname == NULL) {
+        return MEM_ALLOC_ERR;
+    }
+    person->role = malloc(role_sz * sizeof(char));
+    if (person->surname == NULL) {
+        return MEM_ALLOC_ERR;
     }
     return 0;
 }
 
-int enter_participant (char *argv[]) {
-    int size = 0;
-    scanf("%d", &size);
-    for (int i = 0; i < size; i++) {
-        getline(&argv[i], 120, stdin);
-    }
-    return 0;
+int mem_free_person(Person* person) {
+    free(person->name);
+    free(person->surname);
+    free(person->role);
 }
 
-int main(int argc, char *argv[]) //int argc, char *argv[]
+void show_surnames(Person* people, size_t n, char* surname) {
+    for (size_t i = 0; i < n; i++) {
+        if (!strcmp(people[i].surname, surname)) {
+            show_person_info(&people[i]);
+            printf("\n");
+        }
+    }
+}
+
+void show_names(Person* people, size_t n, char* name) {
+    for (size_t i = 0; i < n; i++) {
+        if (!strcmp(people[i].name, name)) {
+            show_person_info(&people[i]);
+            printf("\n");
+        }
+    }
+}
+
+void show_roles(Person* people, size_t n, char* role) {
+    for (size_t i = 0; i < n; i++) {
+        if (!strcmp(people[i].role, role)) {
+            show_person_info(&people[i]);
+            printf("\n");
+        }
+    }
+}
+
+int main()
 {
-    participants* particips = NULL;
-    char *particip_data[] = NULL;
-    size_t particip_count = 0;
-    if (argc > 0) {
-        particip_data = argv;
-    } else {
-        
+    int n = 1;
+    printf("Enter count of project participants: ");
+    scanf("%d", &n);
+    Person* people = malloc(n * sizeof(Person));
+    for (int i = 0; i < n; i++) {
+        mem_alloc_person(&people[i], name_size, surname_size, role_size);
+        if (1) {
+            printf("Enter name: ");
+            scanf("%20s", people[i].name);
+            printf("Enter surname: ");
+            scanf("%30s", people[i].surname);
+            printf("Enter role: ");
+            scanf("%15s", people[i].role);
+            printf("Enter importance level");
+            scanf("%zu", &people[i].importance);
+        } else {
+            printf("Mem alloc error, check your pc");
+        }
     }
-    make_participants(argc, particips, particip_data);
-    
-    char* command_buffer = NULL;
-    size_t cmd_buffer_size = 80;
-    size_t char_cnt = 0;
-    command_buffer = (char*)malloc(cmd_buffer_size * sizeof(char));
-    printf("Enter participant: ");
-    char_cnt = getline(&command_buffer, &cmd_buffer_size, stdin);
-    participants particip1;
-    enter_participant(command_buffer, &particip1);
-    print_particip_info(&particip1);
-    help();
-    
-    
-    printf("Please, enter a command:\n");
-    do {
-        char_cnt = getline(&command_buffer, &cmd_buffer_size, stdin);
-        printf("Please, enter a command:\n");
-    } while (char_cnt > 0);
+    size_t cmd_line_size = 10;
+    char* command_line = malloc(cmd_line_size * sizeof(char));
+    printf("Command list is: EXIT - quit from program, SURNAME | NAME | ROLE <text> - output\n");
+    printf("Enter a command: ");
+    while (scanf("%10s", command_line)) {
+        if (!strcmp(command_line, "SURNAME")) {
+            printf("Enter surname: ");
+            char surname[surname_size];
+            scanf("%30s", surname);
+            show_surnames(people, n, surname );
+        } else if (!strcmp(command_line, "NAME")) {
+            printf("Enter name: ");
+            char name[name_size];
+            scanf("%20s", name);
+            show_names(people, n, name );
+        } else if (!strcmp(command_line, "ROLE")) {
+            printf("Enter role: ");
+            char role[role_size];
+            scanf("%15s", role);
+            show_roles(people, n, role );
+        } else if (!strcmp(command_line, "EXIT")) {
+            printf("Exiting from program");
+            for (size_t i = 0; i < n; i++) {
+                mem_free_person(&people[i]);
+            }
+            free(people);
+            return 0;
+        } else {
+            printf("There is no such command as %10s", command_line);
+        }
+        printf("Enter a command: ");
+    }
     return 0;
 }
